e9541ccb15e3891a578cfd2537d7f24772cf4e73
Martín Volpe <martin.volpe@gmail.com>
2018-04-28 17:21:09 +0400

    Back-end edge cases resolved

--- webapp/Pics.py
+++ webapp/Pics.py
@@ -1,16 +1,25 @@
 from urllib import request
 import json
-
+from urllib.parse import urlencode
 
 ## parameters:
 ##word: the phrase to be converted to pics
 ##n: number of pics
 def word_to_pics(word, n):
-   array= []
-   rawData = request.urlopen('https://www.googleapis.com/customsearch/v1?key=AIzaSyAvlXs6UPjILP-KW2kU9DTRfRWn1VIbqso&cx=005548162878739948931:tn-uu7o72gg&searchType=image&safe=high&q='+word).read()
+   array= [word]
+   encoded_word = urlencode({'q': word})
+   
+   print("request to fetch pictures:", encoded_word)
+   
+   rawData = request.urlopen(
+   		'https://www.googleapis.com/customsearch/v1?key=AIzaSyAvlXs6UPjILP-KW2kU9DTRfRWn1VIbqso&cx=005548162878739948931:tn-uu7o72gg&searchType=image&safe=high&q='+encoded_word
+   		).read()
    jsonData = json.loads(rawData)
-   for i in range(0,n):
-      array.append(jsonData['items'][i]['link'])
+   print("response from image server:", jsonData)
+
+   if int(jsonData['searchInformation']['totalResults']) > 0:
+	   for i in range(0,n):
+	      array.append(jsonData['items'][i]['link'])
 
    return array
 

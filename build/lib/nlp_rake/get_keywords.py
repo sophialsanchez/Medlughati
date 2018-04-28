9c027b72300a049bc627794fd6a309ad9bdb57e1
sophialsanchez <sophia.l.sanchez@gmail.com>
2018-04-28 11:20:29 +0400

    Initial commit for get_keywords.py

---
+++ build/lib/nlp_rake/get_keywords.py
@@ -0,0 +1,16 @@
+import rake
+import operator
+
+"""
+A function to determine which keywords 
+in a given sentence should be displayed.
+"""
+
+example_sentences = [
+	"I woke up this morning and my head really hurt.",
+	"I had major surgery on my throat yesterday.",
+	"My child has been feeling sick for a week, and I'm not sure what's wrong with her."
+]
+
+def get_keywords(sentence):
+	pass
